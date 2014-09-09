1fa19c0d96eca0cf8daf4d9ce10f8d12356aeb6b
Sathvik P <psathvik@gmail.com>
2014-09-10 01:39:59 +0530

    jsonform+classes+

--- app/controllers/account/AccountController.php
+++ app/controllers/account/AccountController.php
@@ -66,6 +66,7 @@ class AccountController extends BaseController {
             $oldAccount = clone $account;
             $account->name = Input::get('name');
             $account->cloudProvider = Input::get('cloudProvider');
+            $account->credentials = json_encode(Input::get('credentials'));
             $account->user_id = Auth::id(); // logged in user id
             //@TODO  based on the json template for provider to populate, load the field and values.
             // Save it in credentials field of table as json.
