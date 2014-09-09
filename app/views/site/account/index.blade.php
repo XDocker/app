613ccb0eb7095fd67fcf67af31b1e92916e46633
vikas.gurrapu <vikas.gurrapu@gmail.com>
2014-09-09 19:26:51 +0530

    accounts

--- app/views/site/account/index.blade.php
+++ app/views/site/account/index.blade.php
@@ -10,15 +10,15 @@
 				<a class="pull-left" href="#">
 				    <img class="media-object img-circle" src="http://placehold.it/260x180" alt="">
 				</a>
-				<a href="#" class="btn btn-inverse pull-right" role="button">Deploy</a>
+				<a href="#" class="btn btn-inverse pull-right" role="button">Edit</a>
 				<div class="media-body">
-					<h6 class="media-heading"><a href="">{{ String::title($account->title) }}</a></h6>
+					<h6 class="media-heading"><a href="">{{ String::title($account->name) }}</a></h6>
 				    <p>
-				    	{{ String::tidy(Str::limit($account->content, 200)) }}
+				    	{{ String::tidy(Str::limit($account->credentials, 200)) }}
 					</p>
 					<hr>
 					<p>
-						<span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $account->date() }}}
+						<span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $account->created_at }}}
 					</p>
 				</div>
 			</div>
