500013944528d0afccaa5ac5c91ac703465272cb
vikas.gurrapu <vikas.gurrapu@gmail.com>
2014-09-09 07:57:27 +0530

    ui and nav

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -49,22 +49,23 @@
 		<!-- To make sticky footer need to wrap in a div -->
 		<div id="wrap">
 		<!-- Navbar -->
-		<div class="navbar navbar-default navbar-inverse navbar-fixed-top">
-			 <div class="container">
+        	
+		<nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+			 <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-target">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
                 </div>
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <div class="collapse navbar-collapse" id="navbar-collapse-target">
                     <ul class="nav navbar-nav">
 						<li {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">Home</a></li>
 					</ul>
 
-                    <ul class="nav navbar-nav pull-right">
+                    <ul class="nav navbar-nav navbar-right">
                         @if (Auth::check())
                         @if (Auth::user()->hasRole('admin'))
                         <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
@@ -80,7 +81,7 @@
 					<!-- ./ nav-collapse -->
 				</div>
 			</div>
-		</div>
+		</nav>
 		<!-- ./ navbar -->
 
 		<!-- Container -->
