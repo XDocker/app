2db829f04f6a11ecc4236d165d76a0696d16b463
Sathvik P <psathvik@gmail.com>
2014-09-13 22:06:54 +0530

    Dummy WS for deployments

--- app/controllers/deployment/DeploymentController.php
+++ app/controllers/deployment/DeploymentController.php
@@ -59,10 +59,33 @@ class DeploymentController extends BaseController {
             $deployment->name = Input::get('name');
             $deployment->cloud_account_id = Input::get('cloud_account_id');
             $deployment->parameters = json_encode(Input::get('parameters'));
-            $deployment->user_id = Auth::id(); // logged in user id
-            //@TODO get and save status from external WS
-            $deployment->status = 'Unknown';
             $deployment->docker_name = Input::get('docker_name');
+            $deployment->user_id = Auth::id(); // logged in user id
+            try {
+                // Get and save status from external WS
+                $process = curl_init(Config::get('local/deployment_api.url'));
+                curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+                curl_setopt($process, CURLOPT_SSL_VERIFYPEER, FALSE);
+                $fields = array(
+                    'parameters' => $deployment->parameters,
+                    'cloud_account' => CloudAccount::findOrFail($deployment->cloud_account_id) ,
+                    'docker_name' => $deployment->docker_name
+                );
+                //url-ify the data for the POST
+                $fields_string = '';
+                foreach ($fields as $key => $value) {
+                    $fields_string.= $key . '=' . $value . '&';
+                }
+                $status = curl_exec($process);
+                curl_setopt($process, CURLOPT_POST, count($fields));
+                curl_setopt($process, CURLOPT_POSTFIELDS, $fields_string);
+                curl_close($process);
+            }
+            catch(Exception $err) {
+                $status = 'Unexpected Error: ' . $err->getMessage();
+                throw new Exception($err->getMessage());
+            }
+            $deployment->status = $status;
             $success = $deployment->save();
             if (!$success) {
                 throw new Exception($deployment->errors());
@@ -73,7 +96,7 @@ class DeploymentController extends BaseController {
             return Redirect::to('/')->with('success', Lang::get('deployment/deployment.deployment_deployment_updated'));
         }
         catch(Exception $e) {
-            return Redirect::intended('/')->with('error', $e->getMessage());
+            return Redirect::back()->with('error', $e->getMessage());
         }
     }
     /**
