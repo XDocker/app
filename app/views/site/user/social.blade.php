cce2ec9d8ef9d4dcafa2b4c2092e8151702bd5b4
Sudhi <sid4it@gmail.com>
2014-09-14 18:36:17 -0500

    Github name modifed.

--- app/views/site/user/social.blade.php
+++ app/views/site/user/social.blade.php
@@ -4,5 +4,5 @@
 </p> 
 <p class="text-center">
 	<a title="LinkedIn" href="{{URL::to('user/social')}}?provider=LinkedIn" class="social-linkedin "><i class="fa fa-linkedin-square fa-4x social-login-icons"></i></a>
-	<a title="Github" href="{{URL::to('user/social')}}?provider=Github" class="social-git"><i class="fa fa-github-square fa-4x"></i></a>
+	<a title="Github" href="{{URL::to('user/social')}}?provider=GitHub" class="social-git"><i class="fa fa-github-square fa-4x"></i></a>
 </p>
