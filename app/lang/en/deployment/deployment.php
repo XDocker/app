c98aab4648c181d7143c17c777b1d13ca9a986b9
Sudhi <sid4it@gmail.com>
2014-09-17 14:29:48 -0500

    verbiage.

--- app/lang/en/deployment/deployment.php
+++ app/lang/en/deployment/deployment.php
@@ -13,6 +13,6 @@ return array(
 	'cp_amazon_aws'            => 'Amazon AWS',
 	'deployment_created'          => 'Deployment created.',
 	'empty_deployments'  		   => 'You do not have any deployments. Create one by clicking on "Add New Cloud Deployment" below',
-	'deployment.account_required'  => 'To deploy, please create the Account'
+	'deployment.account_required'  => 'To deploy, please create the Account first'
 
 );
