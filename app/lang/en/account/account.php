400f65d093a40a06fd8cfabe38fabae9fac91dd9
Sudhi <sid4it@gmail.com>
2014-09-17 02:58:00 -0500

    sanity check for empty accounts

--- app/lang/en/account/account.php
+++ app/lang/en/account/account.php
@@ -14,5 +14,6 @@ return array(
 	'account_updated'          => 'Account updated.',
 	'account_created'          => 'Account created.',
 	'account_auth_failed'	   => 'Account authentication failed. Please check credentials',
+	'account.empty_accounts'  => 'You do not have any accounts. Create one by clicking on "Add New Cloud Account" below'
 
 );
--- app/views/site/account/index.blade.php
+++ app/views/site/account/index.blade.php
@@ -5,29 +5,34 @@
 
 <div class="media-block">
 	<ul class="list-group">
-@foreach ($accounts as $account)
-  		<li class="list-group-item">
-			<div class="media">
-				<span class="pull-left" href="#">
-				    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$account->cloudProvider.'.logo')) }}" alt="{{ $account->cloudProvider }}" />
-				</span>
-				<form class="pull-right" method="post" action="{{ URL::to('account/' . $account->id . '/delete') }}">
-					<!-- CSRF Token -->
-					<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-					<!-- ./ csrf token -->
-					<button type="submit" class="btn btn-warning pull-right" role="button">Delete</button>
-				</form>
-				<a href="{{ URL::to('account/' . $account->id . '/edit') }}" class="btn btn-success pull-right" role="button">Edit</a>
-				<div class="media-body">
-					<h4 class="media-heading">{{ String::title($account->name) }}</h4>
-					<p>
-						<span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $account->created_at }}}
-					</p>
-				</div>
-			</div>
-		</li>
-@endforeach
+		@if(!empty($accounts)) 
+			@foreach ($accounts as $account)
+	  			<li class="list-group-item">
+					<div class="media">
+						<span class="pull-left" href="#">
+						    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$account->cloudProvider.'.logo')) }}" alt="{{ $account->cloudProvider }}" />
+						</span>
+						<form class="pull-right" method="post" action="{{ URL::to('account/' . $account->id . '/delete') }}">
+							<!-- CSRF Token -->
+							<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+							<!-- ./ csrf token -->
+							<button type="submit" class="btn btn-warning pull-right" role="button">Delete</button>
+						</form>
+						<a href="{{ URL::to('account/' . $account->id . '/edit') }}" class="btn btn-success pull-right" role="button">Edit</a>
+						<div class="media-body">
+							<h4 class="media-heading">{{ String::title($account->name) }}</h4>
+							<p>
+								<span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $account->created_at }}}
+							</p>
+						</div>
+					</div>
+				</li>
+			@endforeach
+		@endif
 	</ul>
+	@if(empty($accounts) || count($accounts) === 0) 
+		<div class="alert alert-info"> {{{ Lang::get('account/account.empty_accounts') }}}</div>
+	@endif
 </div>
 <div>
 <a href="{{ URL::to('account/create') }}" class="btn btn-primary pull-right" role="button">Add New Cloud Account</a>
