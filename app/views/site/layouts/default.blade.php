f4b1495e4507432ed66f754301ce0507f19409a2
Sudhi <sid4it@gmail.com>
2014-09-17 07:55:50 -0500

    glyphicon-cog

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -69,7 +69,7 @@ But when you are vetting a service, as to how it looks and works, you may need t
 		                        @endif
 		                        <li class="dropdown">
 		                        	<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-	    								<span class="glyphicon glyphicon-cogwheel"></span> Services	<span class="caret"></span>
+	    								<span class="glyphicon glyphicon-cog"></span> Services	<span class="caret"></span>
 	    							</a>
 		                        	<ul class="dropdown-menu">
 	    								<li><a href="{{{ URL::to('account') }}}"><span class="glyphicon glyphicon-wrench"></span> Accounts</a></li>
