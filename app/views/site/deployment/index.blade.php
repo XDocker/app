0a0cad01e73670b5acaa82a55914894990974f5a
Sathvik P <psathvik@gmail.com>
2014-09-18 23:14:40 +0530

    CloudProvider data on deployments screen + UI tweak

--- app/views/site/deployment/index.blade.php
+++ app/views/site/deployment/index.blade.php
@@ -17,9 +17,10 @@
 			@foreach ($deployments as $deployment)
 	  			<li class="list-group-item">
 					<div class="media">
-						<span class="pull-left" href="#">
+						<a href="{{ URL::to('account/'.$deployment->cloud_account_id.'/edit') }}" class="pull-left" href="#">
 						    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$deployment->cloudProvider.'.logo')) }}" alt="{{ $deployment->cloudProvider }}" />
-						</span>
+						    <p class="text-center">{{{$deployment->accountName}}}</p>
+						</a>
 						<form class="pull-right" method="post" action="{{ URL::to('deployment/' . $deployment->id . '/delete') }}">
 							<!-- CSRF Token -->
 							<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
@@ -30,8 +31,6 @@
 							
 							<h4 class="media-heading">{{ String::title($deployment->name) }}</h4>
 							<p>
-								<span title="Account"><span class="fa fa-wrench"></span> <a href="{{ URL::to('account/'.$deployment->cloud_account_id.'/edit') }}">{{{$deployment->accountName}}}</a></span>
-								|
 								<span title="Created At"><span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $deployment->created_at }}}</span>
 							</p>
 						</div>
