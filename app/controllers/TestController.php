3f7dd76398b57215b18ec8d02da1c8e193d025d7
Sudhi <sid4it@gmail.com>
2014-09-22 00:00:53 -0500

    RawCurl

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -37,10 +37,29 @@ class TestController extends BaseController {
     public function getIndex() {
     	echo 'Test Controller';
 		
-		 $engine = new xDockerEngine();
-		 $orchestrationParams = Config::get('orchestration');
-		 echo '<pre>';
-		 print_r($orchestrationParams);
-		 //$engine ->server($orchestrationParams)
+		$orchestrationParams = Config::get('orchestration');
+		echo '<pre>';
+		print_r($orchestrationParams);
+		$process = curl_init(Config::get('orchestration.endpoint_ip') . Config::get('orchestration.register'));
+        curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $fields = array(
+                    'username' => 'sudhi',
+                    'password' => 'sudhi'
+        );
+                //url-ify the data for the POST
+                $fields_string = '';
+                foreach ($fields as $key => $value) {
+                    $fields_string.= $key . '=' . $value . '&';
+                }
+        $status = curl_exec($process);
+        curl_setopt($process, CURLOPT_POST, count($fields));
+        curl_setopt($process, CURLOPT_POSTFIELDS, $fields_string);
+        curl_close($process);
+		
+		print_r($status);
+		//$engine ->server($orchestrationParams)
 	}
 }
