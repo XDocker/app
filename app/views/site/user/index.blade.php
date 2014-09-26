ec30c65555697676593f93d0045bb788813c70de
sseshachala <sid4it@gmail.com>
2014-09-25 23:43:46 -0500

    User made readonly

--- app/views/site/user/index.blade.php
+++ app/views/site/user/index.blade.php
@@ -27,7 +27,7 @@
         <div class="form-group {{{ $errors->has('username') ? 'error' : '' }}}">
             <label class="col-md-2 control-label" for="username">Username</label>
             <div class="col-md-10">
-                <input class="form-control" type="text" name="username" id="username" value="{{{ Input::old('username', $user->username) }}}" />
+                <input class="form-control" type="text" name="username" id="username" value="{{{ Input::old('username', $user->username) }}}" readonly/>
                 {{ $errors->first('username', '<span class="help-inline">:message</span>') }}
             </div>
         </div>
