f8c835413115cc83fed95065a349cd0a3e8e9cac
sseshachala <sid4it@gmail.com>
2014-09-27 07:27:59 -0500

    verbiage.

--- app/lang/en/user/user.php
+++ app/lang/en/user/user.php
@@ -20,7 +20,7 @@ return array(
 	'update'               => 'Update',
 	'user_account_is_not_confirmed'          => 'User Account is not confirmed.',
 	'user_account_updated'          => 'User Account updated.',
-	'user_account_created'          => 'User Account created.',
+	'user_account_created'          => 'User Account created. Check your email for the confirmation link and click on the link to confirm your account.',
 
 	'signup_social' => 'Signup with these instead:',
 	'password_confirm' => 'Password Confirm',
