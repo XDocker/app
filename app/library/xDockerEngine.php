37bff44903523ca0f4c39a2dcd1fab5ccb377e61
sseshachala <sid4it@gmail.com>
2014-09-29 01:39:24 -0500

    Register method

--- app/library/xDockerEngine.php
+++ app/library/xDockerEngine.php
@@ -44,42 +44,34 @@ class xDockerEngine {
     }
     
     public static function register($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['register'], $data);
     }
     
     public static function authenticate($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['authenticate'], $data);
     }
     
     public static function run($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['run'], $data);
     }
     
     public static function instance($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['instance'], $data);
     }
     
     public static function getDeploymentStatus($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['getDeploymentStatus'], $data);
     }
     
     public static function getLog($data) {
-        self::init();
-        return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['getLog'], $data);
+      return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['getLog'], $data);
     }
     
     public static function uploadKey($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['uploadKey'], $data);
     }
     
     public static function downloadKey($data) {
-        self::init();
         return self::request(self::$orchestrationParams['endpoint_ip'] . '/' . self::$orchestrationParams['downloadKey'], $data);
     }
 }
