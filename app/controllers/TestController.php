74787468de4e5b83486b8b99ac092517056684d8
sseshachala <sid4it@gmail.com>
2014-09-29 03:06:25 -0500

    minor.

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -62,7 +62,24 @@ class TestController extends BaseController {
 		$orchestrationParams = Config::get('orchestration');
 		echo '<pre>';
 		print_r($orchestrationParams);
-		
+		$process = curl_init();
+		curl_setopt($process, CURLOPT_URL, 'http://104.131.38.159:5000' . '/register');
+        curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($process, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $fields = array(
+                    'username' => 'sudhi', 
+                    'password' => 'sudhi'
+        );
+                //url-ify the data for the POST
+	curl_setopt($process, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($process, CURLOPT_POSTFIELDS, json_encode($fields));                                                                  
+	curl_setopt($process, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($process, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen(json_encode($fields)))                                                                       
+);                                             
+       $status = curl_exec($process);
+       curl_close($process); 
 		
 		print_r($status);
 		//$engine ->server($orchestrationParams)
