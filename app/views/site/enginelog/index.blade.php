613054106f2c30bc94546ca29cd164709c5a173b
sseshachala <sid4it@gmail.com>
2014-09-29 08:53:47 -0500

    Verbiages.

--- app/views/site/enginelog/index.blade.php
+++ app/views/site/enginelog/index.blade.php
@@ -6,7 +6,7 @@
 <div class="page-header">
 	<div class="row">
 		<div class="col-md-9">
-			<h4>{{{ Lang::get('account/account.your_accounts') }}}</h4>
+			<h4>{{{ Lang::get('enginelog/enginelog.your_logs') }}}</h4>
 		</div>
 	</div>
 </div>
