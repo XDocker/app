92e92f63bdb8be2fc54a5804fcbad4551c689d54
sseshachala <sid4it@gmail.com>
2014-09-29 14:46:39 -0500

    deploymentId

--- app/views/site/deployment/index.blade.php
+++ app/views/site/deployment/index.blade.php
@@ -21,7 +21,7 @@
 						    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$deployment->cloudProvider.'.logo')) }}" alt="{{ $deployment->cloudProvider }}" />
 						    <p class="text-center">{{{$deployment->accountName}}}</p>
 						</a>
-						<form class="pull-right" method="post" action="{{ URL::to('deployment/' . $deployment->id . '/delete') }}">
+						<form class="pull-right" method="post" action="{{ URL::to('deployment/' . $deployment->deploymentId . '/delete') }}">
 							<!-- CSRF Token -->
 							<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 							<!-- ./ csrf token -->
