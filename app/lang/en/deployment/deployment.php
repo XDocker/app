0081bc79bc58f35445d94c52e77aab1fe5122c81
sseshachala <sid4it@gmail.com>
2014-09-30 13:15:00 -0500

    Deployment.

--- app/lang/en/deployment/deployment.php
+++ app/lang/en/deployment/deployment.php
@@ -13,6 +13,7 @@ return array(
 	'cp_amazon_aws'            => 'Amazon AWS',
 	'deployment_created'          => 'Deployment created.',
 	'empty_deployments'  		   => 'You do not have any deployments. Create one by clicking on "Add New Cloud Deployment" below',
-	'deployment.account_required'  => 'To deploy, please create the Account first'
+	'deployment.account_required'  => 'To deploy, please create the Account first',
+	'deployment_updated'   => 'Deployment Updated successfully.',
 
 );
