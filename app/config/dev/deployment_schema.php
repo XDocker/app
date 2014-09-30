f831de8f312ee30226bda6547f651c9130a39f8b
sseshachala <sid4it@gmail.com>
2014-09-30 01:50:32 -0500

    Dropdown list.

--- app/config/dev/deployment_schema.php
+++ app/config/dev/deployment_schema.php
@@ -7,19 +7,54 @@
 // Schema for the Provider specific fields, will be converted into JSON and used on the front-end with https://github.com/joshfire/jsonform
 
 return array(
-    /*'Amazon AWS' => array(
-        'parameters[securityGroupId]' => array(
-            'type' => 'string',
-            'title' => 'Security Group',
-            'required' => true
+    'Amazon AWS' => array(
+        'parameters[instanceRegion]' => array(
+            'type' => 'selectfieldset',
+            'title' => 'Region',
+            'required' => true,
+                  "items"=> array(
+                  		array(
+			          "key"=> "us-east-1",
+			          "legend"=> "us-east-1"
+			        ),
+			        array(
+			          "key"=> "us-west-1",
+			          "legend"=> "us-west-1"
+			        ),
+			         array(
+			          "key"=> "us-west-2",
+			          "legend"=> "us-west-2"
+			        ),
+			        array(
+			          "key"=> "ap-northeast-1",
+			          "legend"=> "ap-northeast-1"
+			        ),
+			         array(
+			          "key"=> "ap-southeast-1",
+			          "legend"=> "ap-southeast-1"
+			        ),
+			         array(
+			          "key"=> "ap-southeast-2",
+			          "legend"=> "ap-southeast-2"
+			        ),
+			         array(
+			          "key"=> "eu-west-1",
+			          "legend"=> "eu-west-1"
+			        ),
+			         array(
+			          "key"=> "sa-east-1",
+			          "legend"=> "sa-east-1"
+			        )
+					
+      			)
         ) ,
-        'parameters[keyPair]' => array(
+        'parameters[instanceType]' => array(
             'type' => 'string',
-            'title' => 'KeyPair',
+            'title' => 'Instance Type',
             'required' => false
         )
     ) ,
-    'Rackspace Cloud' => array(
+    /*'Rackspace Cloud' => array(
         'parameters[securityGroupId]' => array(
             'type' => 'string',
             'title' => 'Security Group',
