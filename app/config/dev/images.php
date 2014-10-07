41ea9512736c386b9eb65676a87ca66cea414181
sseshachala <sid4it@gmail.com>
2014-10-07 10:09:23 -0500

    Only ubuntu images.

--- app/config/dev/images.php
+++ app/config/dev/images.php
@@ -9,6 +9,20 @@
 return array(
     'Amazon AWS' =>
 				array(
+						'us-east-1' => 		array('Ubuntu 14.04:ami-8caa1ce4'),
+						'us-west-1' => 		array('Ubuntu 14.04:ami-696e652c'),
+						'us-west-2' => 		array('Ubuntu 14.04:ami-cd5311fd'),
+						'eu-west-1' =>  	array('Ubuntu 14.04:ami-00b11177'),
+						'sa-east-1' => 		array('Ubuntu 14.04:ami-79d26764'),
+						'ap-northeast-1' => array('Ubuntu 14.04:ami-d54b60d4'),
+						'ap-southeast-1' => array('Ubuntu 14.04:ami-24e7c076'),
+						'ap-southeast-2' => array('Ubuntu 14.04:ami-2111731b'),
+						'cn-north-1' =>  	array('Ubuntu 14.04:ami-9e42d0a7'),
+						)
+				);
+				
+/*
+array(
 						'us-east-1' => array('Ubuntu 14.04:ami-8caa1ce4', 'CentOS 6.5:ami-8997afe0', 'Amazon Linux:ami-50842d38'),
 						'us-west-1' => array('Ubuntu 14.04:ami-696e652c', 'CentOS 6.5:ami-b6bdde86', 'Amazon Linux:ami-c7a8a182'),
 						'us-west-2' => array('Ubuntu 14.04:ami-cd5311fd','CentOS 6.5:ami-1a013c5f','Amazon Linux:ami-af86c69f'),
@@ -19,34 +33,4 @@ return array(
 						'ap-southeast-2' =>  array('Ubuntu 14.04:ami-2111731b', 'CentOS 6.5:ami-81294380', 'Amazon Linux:ami-8b4724b1'),
 						'cn-north-1' =>  array('Ubuntu 14.04:ami-9e42d0a7','Amazon Linux:ami-a857c591'),
 						)
-				);
-
-/*
-US East (Virginia)	       					ami-8997afe0
-US West (Oregon)	       							ami-b6bdde86
-US West (Northern California) 	ami-1a013c5f
-EU West (Ireland)	        					ami-42718735
-Asia Pacific (Singapore)						ami-a08fd9f2
-Asia Pacific (Sydney)						ami-e7138ddd
-Asia Pacific (Tokyo)	        					ami-81294380
-South America (Sao Paulo)			ami-7d02a260
-
-ap-northeast-1 		ami-d54b60d4
-ap-southeast-1 		ami-24e7c076
-eu-west-1 			ami-00b11177
-sa-east-1 			ami-79d26764
-us-east-1 			, 'ami-8caa1ce4
-us-west-1 			ami-696e652c
-ap-southeast-2 		ami-2111731b
-us-west-2 			ami-cd5311fd
-
-ap-northeast-1 		, 'ami-21072820
-ap-southeast-1 		, 'ami-20e1c572
-eu-west-1 			ami-aa8f28dd
-sa-east-1 			ami-9d6cc680
-us-east-1 			, 
-us-west-1 			ami-c7a8a182
-ap-southeast-2 		ami-8b4724b1
-us-west-2 			ami-af86c69f
- * 
- */
+*/
