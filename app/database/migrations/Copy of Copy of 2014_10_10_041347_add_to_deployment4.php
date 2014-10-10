0decfa07e6e21def75025cffa88a1d76f180f3c5
sseshachala <sid4it@gmail.com>
2014-10-10 11:09:17 -0500

    deployments and account has soft delete function.

---
+++ app/database/migrations/Copy of Copy of 2014_10_10_041347_add_to_deployment4.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddToDeployment4 extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('deployments', function(Blueprint $table)
+		{
+			$table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('deployments', function(Blueprint $table)
+		{
+			$table->dropColumn('deleted_at');
+		});
+	}
+
+}
