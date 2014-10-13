bcd534b0e19d8836b17a0be809c91387c93a4209
sseshachala <sid4it@gmail.com>
2014-10-13 09:34:46 -0500

    Anchor link

--- app/views/site/generic_view.blade.php
+++ app/views/site/generic_view.blade.php
@@ -48,7 +48,8 @@
 								if(in_array($deployment->status, array('Completed', 'start', 'stop')))
 									{
 										$url = URL::to('deployment/'.$deployment->id.'/instanceAction');
-										echo $result->instance_id . ' | ' .xDockerEngine::getProtocol($deployment->docker_name). $result->public_dns . '<br/>';
+										$anchor = '<a href="'.xDockerEngine::getProtocol($deployment->docker_name). $result->public_dns .'">App link</a>';
+										echo $result->instance_id . ' | ' .$anchor . '<br/>';
 										echo '<a href="#" onclick="start(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')">Start</a> |'  .
 										'<a href="#" onclick="stop(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')">Stop</a> |'  .
 										'<a href="#" onclick="restart(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')">Restart</a>'  ;
