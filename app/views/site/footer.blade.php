270e6fdc5955b69c5eceb5623109e858c633b765
sseshachala <sid4it@gmail.com>
2014-10-13 01:02:38 -0500

    Github modification

--- app/views/site/footer.blade.php
+++ app/views/site/footer.blade.php
@@ -11,9 +11,8 @@
 		      			<a href="https://www.linkedin.com/groups/Xdocker-Users-6789160?gid=6789160"><img width="32px" height="32px" src="{{{ asset('assets/img/LinkedIn.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		      			<a href="https://www.facebook.com/pages/XDocker/687711791303636"><img width="32px" height="32px" src="{{{ asset('assets/img/facebook.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		      			<a href="https://www.twitter.com/xdocker"><img width="32px" height="32px" src="{{{ asset('assets/img/Twitter_logo.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
+		      			<iframe src="http://ghbtns.com/github-btn.html?user=xdocker&repo=app&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
+
 		      	</p>
-		        <p class="text-center">
-		        	<iframe src="http://ghbtns.com/github-btn.html?user=xdocker&repo=app&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
-		        </p>
 	</div>
 </footer>
