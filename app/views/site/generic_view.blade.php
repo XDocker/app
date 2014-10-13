85dec02fc38c9fa09a89a6cb5205c579b93f9e90
sseshachala <sid4it@gmail.com>
2014-10-12 23:37:25 -0500

    Docker name logo

--- app/views/site/generic_view.blade.php
+++ app/views/site/generic_view.blade.php
@@ -20,11 +20,16 @@
 			?>
 	  			<li class="list-group-item">
 					<div class="media">
-						
-						<a href="{{ URL::to('account/'.$deployment->cloud_account_id.'/edit') }}" class="pull-left" href="#">
-						    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$deployment->cloudProvider.'.logo')) }}" alt="{{ $deployment->cloudProvider }}" />
-						    <p class="text-center">{{{$deployment->accountName}}}</p>
-						</a>
+						<p>
+							<a href="{{ URL::to('account/'.$deployment->cloud_account_id.'/edit') }}" class="pull-left" href="#">
+							    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.Config::get('provider_meta.'.$deployment->cloudProvider.'.logo')) }}" alt="{{ $deployment->cloudProvider }}" />
+							    <p class="text-center">{{{$deployment->accountName}}}</p>
+							</a> |
+							<a href="#" class="pull-left" href="#">
+							    <img class="media-object img-responsive" src="{{ asset('/assets/img/providers/'.xDockerEngine::getLogo($deployment->docker_name) }}" alt="{{ $deployment->docker_name }}" />
+							    <p class="text-center">{{{$deployment->docker_name}}}</p>
+							</a>
+						</p>
 						<form class="pull-right" method="post" action="{{ URL::to('deployment/' . $deployment->id . '/refresh') }}">
 							<!-- CSRF Token -->
 							<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
