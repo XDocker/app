87e23e576c0316e63644e7b489572a320869e4e7
sseshachala <sid4it@gmail.com>
2014-10-13 15:56:19 -0500

    softdeletes.

--- app/database/migrations/2014_10_10_041347_add_to_deployment4.php
+++ app/database/migrations/2014_10_10_041347_add_to_deployment4.php
@@ -14,7 +14,7 @@ class AddToDeployment4 extends Migration {
 	{
 		Schema::table('deployments', function(Blueprint $table)
 		{
-			$table->softDeletes();
+			//->softDeletes();
 		});
 	}
 
@@ -28,7 +28,7 @@ class AddToDeployment4 extends Migration {
 	{
 		Schema::table('deployments', function(Blueprint $table)
 		{
-			$table->dropColumn('deleted_at');
+			//$table->dropColumn('deleted_at');
 		});
 	}
 
