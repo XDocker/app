0c4501aaf6a02bcb6d594a14e49b087216e635fc
sseshachala <sid4it@gmail.com>
2014-10-14 10:48:54 -0500

    Label adjustment

--- app/library/UIHelper.php
+++ app/library/UIHelper.php
@@ -20,15 +20,16 @@ Important	<span class="label label-important">Important</span>
 Info	<span class="label label-info">Info</span>
 Inverse	<span class="label label-inverse">Inverse</span>
 		 */
-		switch($status)
+		switch(strtolower($status))
 		{
-			case 'Completed' 	: return '<span class="label label-success">'.$status.'</span>'; break;
-			case 'In Progress'  : return '<span class="label">'.$status.'</span>'; break;
-			case 'Started' 		: 
+			case 'completed' 	: return '<span class="label label-success">'.$status.'</span>'; break;
+			case 'in progress'  : return '<span class="label">'.$status.'</span>'; break;
 			case 'start' 		: 
 			case 'started' 		: return '<span class="label label-info">'.$status.'</span>'; break;
-			case 'Stop' 		: return '<span class="label label-inverse">'.$status.'</span>'; break;
+			case 'stop' 		: return '<span class="label label-inverse">'.$status.'</span>'; break;
 			case 'failed' 		: return '<span class="label label-important">'.$status.'</span>'; break;
+			default:			  return '<span class="label label-info">'.$status.'</span>'; break;
+								
 		}
 	}
 	
