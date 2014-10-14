e999c7c9c7a281ade31aa933abc970d7b20944c2
sseshachala <sid4it@gmail.com>
2014-10-14 11:25:16 -0500

    Status label.

--- app/library/UIHelper.php
+++ app/library/UIHelper.php
@@ -26,8 +26,8 @@ Inverse	<span class="label label-inverse">Inverse</span>
 			case 'In Progress'  : return '<span class="label">'.$status.'</span>'; break;
 			case 'start' 		: 
 			case 'started' 		: return '<span class="label label-info">'.$status.'</span>'; break;
-			case 'stop' 		: return '<span class="label label-inverse">'.$status.'</span>'; break;
-			case 'failed' 		: return '<span class="label label-important">'.$status.'</span>'; break;
+			case 'stop' 		: return '<span class="label label-warning">'.$status.'</span>'; break;
+			case 'failed' 		: return '<span class="label label-danger">'.$status.'</span>'; break;
 			default:			  return '<span class="label label-info">'.$status.'</span>'; break;
 								
 		}
