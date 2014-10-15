f1cd8431aaf3266a8b8b6c2a8f0233b945b59006
sseshachala <sid4it@gmail.com>
2014-10-15 17:09:15 -0500

    Footer adjustments

--- app/views/site/footer.blade.php
+++ app/views/site/footer.blade.php
@@ -1,17 +1,19 @@
 <footer id="footer" class="navbar navbar-default navbar-static-bottom">
 	<hr>
 	<div class="container">
-		<p class="muted credit text-center">
-			<img src="{{{ asset('assets/ico/favicon.ico') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/>
-		    <a href="https://www.xervmon.com">{{{ Lang::get('site.footertitle') }}}</a>
-		</p>
-		<p class="text-right"> <strong>{{{ Lang::get('site.follow_us') }}}</strong></p>
+		
+		<div class="row">
+			<p class="muted credit text-center"><img src="{{{ asset('assets/ico/favicon.ico') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/>
+		    	<a href="https://www.xervmon.com">{{{ Lang::get('site.footertitle') }}}</a>
+		 	</p>  
 		<p class="pull-right">
+			<strong>{{{ Lang::get('site.follow_us') }}}</strong>
 			<a href="https://github.com/XDocker"><img width="25px" height="25px" src="{{{ asset('assets/img/providers/GitHub-Mark-32px.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		    <a href="https://www.linkedin.com/groups/Xdocker-Users-6789160?gid=6789160"><img width="25px" height="25px" src="{{{ asset('assets/img/providers/LinkedIn.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		    <a href="https://www.facebook.com/pages/XDocker/687711791303636"><img width="25px" height="25px" src="{{{ asset('assets/img/providers/facebook.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		    <a href="https://www.twitter.com/xdocker"><img width="25px" height="25px" src="{{{ asset('assets/img/providers/Twitter_logo.png') }}}" alt="{{{ Lang::get('site.footertitle') }}}"/></a>
 		</p>
+		 </div>
 		<!--
 		<p class="text-center">
 			<iframe src="http://ghbtns.com/github-btn.html?user=xdocker&repo=app&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="170" height="30"></iframe>
