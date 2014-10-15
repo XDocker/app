a3534fcbffc10b78f56d66a520d4f4aae95421d1
sseshachala <sid4it@gmail.com>
2014-10-15 09:00:28 -0500

    Instance types.

--- app/config/dev/deployment_schema.php
+++ app/config/dev/deployment_schema.php
@@ -31,10 +31,20 @@ return array(
             'title' => 'Instance Type',
             'required' => false,
             'enum' => array(
-                'm3.medium',
-                'm3.large',
-                'm3.xlarge',
-                'm3.2xlarge'
+                "m1.small",
+	            "m1.medium",
+	            "m1.large",
+	            "m1.xlarge",
+	            "m2.xlarge",
+	            "m2.2xlarge",
+	            "m3.xlarge",
+	            "m3.2xlarge",
+	            "m2.4xlarge",
+	            "c1.medium",
+	            "c1.xlarge",
+	            "cc1.4xlarge",
+	            "cc2.8xlarge",
+	            "cg1.4xlarge"
             )
         ),
 		 'parameters[ports]' => array(
