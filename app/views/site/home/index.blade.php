3582e640567007af3b38ff827b8d94fdd6e722fe
sseshachala <sid4it@gmail.com>
2014-10-16 13:33:03 -0500

    home page fix

--- app/views/site/home/index.blade.php
+++ app/views/site/home/index.blade.php
@@ -21,7 +21,7 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-heading">{{{ Lang::get('site.name') }}}</h2>
                     <h5 class="section-subheading text-muted">{{{ Lang::get('site.tagline') }}}</h5>
-                    <h7 class="section-subheading text-muted btn btn-success">{{{ Lang::get('site.launch') }}}</h7>
+                    <strong class="section-subheading text-muted btn btn-success">{{{ Lang::get('site.launch') }}}</strong>
                 		
 		</div>
             </div>
