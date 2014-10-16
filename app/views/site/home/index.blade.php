d2e9be5293bf00ad5bbe856b1a976af1cb4feba0
sudhi <sid4it@gmail.com>
2014-10-16 14:22:48 -0400

    home page cleaned up

--- app/views/site/home/index.blade.php
+++ app/views/site/home/index.blade.php
@@ -22,14 +22,9 @@
                     <h2 class="section-heading">{{{ Lang::get('site.name') }}}</h2>
                     <h5 class="section-subheading text-muted">{{{ Lang::get('site.tagline') }}}</h5>
                     <h7 class="section-subheading text-muted btn btn-success">{{{ Lang::get('site.launch') }}}</h7>
-                    
-                    <br/><br/>
-        	 			@include('laravel-newsletter-signup::signup')
                 		
-				</div>
+		</div>
             </div>
-            <br/>
-            <br/>
             <div class="row text-center">
                 <div class="col-md-4">
                     <span class="fa-stack fa-4x">
@@ -61,11 +56,9 @@
                 </div>
             </div>
         </div>
-        <br/>
         <div class="alert alert-info text-center">
         	<h4>{{{ Lang::get('home.xervmon_intro') }}}</h4>
         	<a target="_blank" href="https://www.xervmon.com/product" class="btn btn-primary">{{{ Lang::get('home.xervmon_call_to_action') }}}</a>
-        	
         </div>
        
     </section>
