9195c2ebd18a43748b46e8f0fb3c36a5af69d5fa
sseshachala <sid4it@gmail.com>
2014-10-17 16:32:05 -0500

    Contact Xervmon to manage this

--- app/views/site/generic_view.blade.php
+++ app/views/site/generic_view.blade.php
@@ -52,7 +52,9 @@
 											$url = URL::to('deployment/'.$deployment->id.'/instanceAction');
 											$anchor = '<a target="_blank" href="'.xDockerEngine::getProtocol($deployment->docker_name). $result->public_dns .xDockerEngine::urlAppend($deployment->docker_name).'">'.xDockerEngine::getDisplayName($deployment->docker_name).'</a>';
 											echo $result->instance_id .xDockerEngine::getDockerUrl($deployment->docker_name) . ' | ' .$anchor . ' | '  .xDockerEngine::documentationUrl($deployment->docker_name) 
-											.' | <a title="Support" alt="Support" class="glyphicon glyphicon-envelope" href="mailto:info@xervmon.com"></a>';
+											.' | <a title="Support" alt="Support" class="glyphicon glyphicon-envelope" href="mailto:support@xervmon.com"></a>'
+											.' | <a title="Contact Xervmon to manage this" alt="Contact Xervmon to manage this" href="mailto:support@xervmon.com">'.asset('assets/ico/favicon.ico').'</a>';
+									
 											
 											//'<a title="Start" href="#" onclick="start(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')"><span class="glyphicon glyphicon-collapse-up"> </span></a> | '  .
 											//'<a title="Stop" href="#" onclick="stop(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')"><span class="glyphicon glyphicon-collapse-down"> </span></a> | '.
