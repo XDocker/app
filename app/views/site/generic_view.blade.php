a1d9d505ed2351e6c2e5235c4da0f382434c6e1b
sseshachala <sid4it@gmail.com>
2014-10-20 07:12:51 -0500

    Download the keys even when app is being initiated.

--- app/views/site/generic_view.blade.php
+++ app/views/site/generic_view.blade.php
@@ -65,8 +65,9 @@
 									{
 										echo  xDockerEngine::getDockerUrl($deployment->docker_name) . ' | ' .xDockerEngine::getDisplayName($deployment->docker_name) . ' | ' .xDockerEngine::documentationUrl($deployment->docker_name).
 										' | <a title="Support" alt="Support" class="glyphicon glyphicon-envelope" href="mailto:support@xervmon.com"></a>'
-										.' | <a title="Contact Xervmon to manage this" alt="Contact Xervmon to manage this" href="mailto:support@xervmon.com"><img src="'.asset('assets/ico/favicon.ico').'"/></a>';
-									
+										.' | <a title="Contact Xervmon to manage this" alt="Contact Xervmon to manage this" href="mailto:support@xervmon.com"><img src="'.asset('assets/ico/favicon.ico').'"/></a>'
+										. ' | <a title="Download" href="#" onclick="downloadKey(\''.$url.'\',\''.$result->instance_id.'\', \''.csrf_token().'\')"><span class="glyphicon glyphicon-download"> </span>Download Pem</a>';
+											
 									}
 									?>
 									
