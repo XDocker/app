50c691e55a334e072f1ab66180009c47654d6454
sseshachala <sid4it@gmail.com>
2014-10-21 17:23:59 -0500

    footer.

--- app/views/site/footer.blade.php
+++ app/views/site/footer.blade.php
@@ -1,4 +1,15 @@
-<div id="footer" class="navbar navbar-default navbar-static-bottom">
+<style>
+.wrapper {
+    min-height: 100%;
+    height: auto !important;
+    height: 100%;
+    margin: 0 auto -155px; /* the bottom margin is the negative value of the footer's height */
+}
+.footer, .push {
+    height: 155px; /* .push must be the same height as .footer */
+}
+</style>
+<div class="footer">
 
 	<p>Copyright (c) 2008</p>
 
