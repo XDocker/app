1d93fc36cf48b0f51c90dd59613ce41945b464cb
sseshachala <sid4it@gmail.com>
2014-10-23 10:52:34 -0500

    Testing production settings.

--- app/config/testing/deployment_schema.php
+++ app/config/testing/deployment_schema.php
@@ -30,12 +30,23 @@ return array(
             'type' => 'string',
             'title' => 'Instance Type',
             'required' => false,
-            'enum' => array(
-                'm3.medium',
-                'm3.large',
-                'm3.xlarge',
-                'm3.2xlarge'
-            )
+            'enum' => array('',
+                "m1.small",
+	            "m1.medium",
+	            "m1.large",
+	            "m1.xlarge",
+	            "m2.xlarge",
+	            "m2.2xlarge",
+	            "m3.xlarge",
+	            "m3.2xlarge",
+	            "m2.4xlarge",
+	            "c1.medium",
+	            "c1.xlarge",
+	            "cc1.4xlarge",
+	            "cc2.8xlarge",
+	            "cg1.4xlarge"
+            ),
+            'event' => 'onchange="loadPrices(this)"'
         ),
 		 'parameters[ports]' => array(
             'type' => 'string',
