a410e364b75c9d64cd03d0626b271b349711423b
sseshachala <sid4it@gmail.com>
2014-10-27 13:17:24 -0500

    minor fix.

--- vendor/fbf/laravel-newsletter-signup/src/models/Signup.php
+++ vendor/fbf/laravel-newsletter-signup/src/models/Signup.php
@@ -1,9 +1,15 @@
 <?php namespace Fbf\LaravelNewsletterSignup;
 
+
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+
 class Signup extends \Eloquent {
 
+	use SoftDeletingTrait;
 	protected $table = 'fbf_newsletter_signups';
 
 	protected $softDelete = true;
+	protected $dates = ['deleted_at'];
 
 }
