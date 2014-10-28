40477d3aa3ee8a78131dbcd12a6b3c3350424e73
sseshachala <sid4it@gmail.com>
2014-10-27 20:12:15 -0500

    moved subscribers under users.

--- app/views/admin/layouts/default.blade.php
+++ app/views/admin/layouts/default.blade.php
@@ -82,8 +82,11 @@
     			<div class="collapse navbar-collapse navbar-ex1-collapse">
     				<ul class="nav navbar-nav">
     					<li{{ (Request::is('admin') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin') }}}"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-    					<li{{ (Request::is('admin/blogs*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/blogs') }}}"><span class="glyphicon glyphicon-list-alt"></span> Blog</a></li>
-    					<li{{ (Request::is('admin/comments*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/comments') }}}"><span class="glyphicon glyphicon-bullhorn"></span> Comments</a></li>
+    					<li{{ (Request::is('admin/blogs*') ? ' class="dropdown active"' : 'dropdown') }}><a href="{{{ URL::to('admin/blogs') }}}"><span class="glyphicon glyphicon-list-alt"></span> Blog</a>>
+    						<ul class="dropdown-menu">
+    							<li{{ (Request::is('admin/comments*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/comments') }}}"><span class="glyphicon glyphicon-bullhorn"></span> Comments</a></li>
+    						</ul>
+    					</li
     					<li{{ (Request::is('admin/accounts*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/accounts') }}}"><span class="glyphicon glyphicon-credit-card"></span> Accounts</a></li>
     					
     					<li{{ (Request::is('admin/subscribers*') ? ' class="active"' : '') }}><a href="{{{ URL::to('admin/subscribers') }}}"><span class="glyphicon glyphicon-user"></span> Subscribers</a></li>
