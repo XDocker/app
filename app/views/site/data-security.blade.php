ae7759fe4fb44c3e9a37f7bb145c0b417b569834
sudhi <sid4it@gmail.com>
2014-11-02 15:04:16 -0600

    Data Security.

--- app/views/site/data-security.blade.php
+++ app/views/site/data-security.blade.php
@@ -9,8 +9,17 @@
 @section('content')
 
 <div class="row">
-  	<div class="col-md-12"><p>
-		At xDocker we make it a priority to keep the client data safe through a defined system of vigorous security practices.  Below are some of the reasons why we can confidently assure you that your data will be secure with Xervmon:
+  	<div class="col-md-12">
+  		<p>
+		At xDocker we make it a priority to keep the client data safe through a defined system of vigorous security practices.  Below are some of the reasons why we can confidently assure you that your data will be secure with xDocker.
+		</p>
+	</div>
+	<div class="col-md-12">
+  		<p>
+		<strong>Secure Data Centers</strong><br/>
+		At xDocker we store our data with some of the most reputable data centers on the market today, such as Amazon AWS. These data centers are both certified with ISO/IEC 27001:2005, SAS70 Type II compliance as well as the PCI DSS Level 1 compliance.
+		If you want to find out more about the security practices at these data centers read, <a href="http://aws.amazon.com/security/">read here</a> for AWS.
+		</p>
 	</div>
 </div>
 @stop
