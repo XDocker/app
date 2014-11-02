853155001cfe23b587ff475ff5491f0d9e37b04c
sudhi <sid4it@gmail.com>
2014-11-02 15:19:39 -0600

    Data security link

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -61,11 +61,8 @@
 	                </div>
 	                <div class="collapse navbar-collapse navbar-ex1-collapse" id="navbar-collapse-target">
 	                	<ul class="nav navbar-nav">
-	                		<li class="dropdown">
-		                        	<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-	    								<span class="glyphicon glyphicon-lock"></span> {{{ Lang::get('site.data_security') }}}	<span class="caret"></span>
-	    							</a>
-	    					</li>
+	                		<li class="active"><a href="{{{ URL::to('data-security') }}}"><span class="glyphicon glyphicon-lock"></span> {{{ Lang::get('site.data_security') }}}</a></li>
+    				
 	                	</ul>	
 	                    <ul class="nav navbar-nav pull-right">
 	                        @if (Auth::check())
