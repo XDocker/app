d62874b76dc4fa2697cecb7704435bae0de56074
sudhi <sid4it@gmail.com>
2014-11-13 16:47:25 -0800

    revert.

--- app/views/site/generic_view.blade.php
+++ app/views/site/generic_view.blade.php
@@ -90,7 +90,9 @@
 								</p>
 								<p>
 									@if($deployment->status == 'Completed' && isset($result->public_dns))
+										@if (strpos($instanceState, 'running') !== false) 
 											{{UIHelper::getContainer(RemoteAPI::Containers($result->public_dns))}}
+										@endif
 									@endif
 								</p>
 			
