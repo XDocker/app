7121b0251058098074ede901f89df0e47f4e9b03
sudhi <sid4it@gmail.com>
2014-12-08 01:34:01 -0600

    Bread crumb

---
+++ app/lang/en/breadcrumb/breadcrumb.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+
+	'home'                    => 'home',
+	'account'                 => 'account',
+	'CreateAccount'           => 'CreateAccount',
+	'EditAccount'             => 'EditAccount',
+	'SecurityGroups'          => 'SecurityGroups',
+	'AWS_Details'             => 'AWS_Details',
+	'AwsInfo'                 => 'AwsInfo',
+	'ChartsData'              => 'ChartsData',
+	'InstanceInfo'            => 'InstanceInfo',
+	'EbsInfo'                 => 'EbsInfo',
+	'SecurityGroupInfo'       => 'SecurityGroupInfo',
+	'KpInfo'                  => 'Key pair Info',
+	'PortPreferences'         => 'PortPreferences',
+	'AddPolicy'               => 'AddPolicy',
+	'EngineLog'               => 'EngineLog',
+	'ServiceStatus'           => 'ServiceStatus',
+	'Reserved'                => 'Reserved',
+	'ReservedInstancePricing' => 'Reserved Instance Pricing',
+	'OndemandInstancePricing' => 'OndemandInstancePricing',
+	'EC2Products'             => 'EC2Products',
+	'Ticket'                  => 'Ticket',
+	'AddTicket'               => 'AddTicket',
+	'DataSecurity'            => 'DataSecurity',
+	'Roadmap'                 => 'Roadmap',
+	'CloudExperts'            => 'CloudExperts',
+	'SubnetsInfo'             => 'SubnetsInfo',
+	'TagsInfo'                => 'TagsInfo',
+	'VpcsInfo'                => 'VpcsInfo',
+
+);
