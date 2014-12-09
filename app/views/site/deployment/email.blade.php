ccd7167211987ec65f05bd3c5498ae2638349b36
sudhi <sid4it@gmail.com>
2014-12-09 13:36:28 -0600

    minor

--- app/views/site/deployment/email.blade.php
+++ app/views/site/deployment/email.blade.php
@@ -7,5 +7,8 @@
 		<h2>Deployment Details: {{ $deploymentName}}</h2>
 
 		<div>{{ $dockerImage }} was deployed using {{ $accountName }}</div>
+		
+		For support, contact </br>
+		<a href="http://support.xervmon.com"> Support</a> or <a href-"mailto:xdocker@xervmon.com">xDocker Team</a>
 	</body>
 </html>
