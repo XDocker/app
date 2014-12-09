bc7744e20c97144a159c3d5515270c4b74428b29
Sathvik P <psathvik@gmail.com>
2014-12-09 20:35:05 +0530

    htaccess tweak for trailing slashes

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
         'Zizaco\Confide\ConfideServiceProvider', // Confide Provider
         'Zizaco\Entrust\EntrustServiceProvider', // Entrust Provider for roles
         'Bllim\Datatables\DatatablesServiceProvider', // Datatables
+        'DaveJamesMiller\Breadcrumbs\ServiceProvider', // Breadcrumbs
 
         /* Uncomment for use in development */
         'Way\Generators\GeneratorsServiceProvider', // Generators
@@ -192,6 +193,7 @@ return array(
         'String'          => 'Andrew13\Helpers\String', // String
         'Carbon'          => 'Carbon\Carbon', // Carbon
         'Datatables'      => 'Bllim\Datatables\Datatables', // DataTables
+        'Breadcrumbs' => 'DaveJamesMiller\Breadcrumbs\Facade', // Breadcrumbs
 
     ),
 
