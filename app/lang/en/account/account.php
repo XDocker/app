0650a78c8503eed80919720ab8d96a49684335eb
karthikhooduku <kchakravarthi@hooduku.com>
2014-12-17 17:53:42 +0530

    Delete pop-up model-- added by chaitra.

--- app/lang/en/account/account.php
+++ app/lang/en/account/account.php
@@ -17,6 +17,6 @@ return array(
 	'your_accounts'			   => 'Your Accounts:',
 	'account_auth_failed'	   => 'Account authentication failed. Please check the credentials provided for the selected cloud provider.',
 	'empty_accounts'  		   => 'You do not have any accounts. Create one by clicking on "Add Account" below',
-	'deployment.account_required'  => 'To deploy, please create <a href="'.URL::to('account/create').'">Account</a> first'
-
+	'deployment.account_required'  => 'To deploy, please create <a href="'.URL::to('account/create').'">Account</a> first',
+     'account_delete'           =>   'Are You Sure  Want To Delete This Account ?. This would also Delete PORT PREFERENCES Associated with this Account.',
 );
